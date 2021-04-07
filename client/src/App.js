ac26be01215c33452417ef702bbdb83079133bf8
huynhquynh-dev <huynhquynh-dev>
2021-04-07 10:22:52 +0700

    Client: Added Router and Bootstrap CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import PageRender from './PageRender'
 
 function App() {
   return (
     <Router>
-		<input type="checkbox" id="theme"/>
-        <div className="App">
-        	<div className="main">
-				<h1>Hello World</h1>
-			</div>
+      <input type="checkbox" id="theme" />
+      <div className="App">
+        <div className="main">
+          <Route exact path="/:page" component={PageRender} />
+          <Route exact path="/:page/:id" component={PageRender} />
         </div>
+      </div>
     </Router>
-    
   );
 }
 
