2f7545345f3c9d92b1d212e0b0ea9b393d3a0070
huynhquynh-dev <huynhquynh-dev>
2021-04-07 22:53:54 +0700

    Client: Added style login

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -23,7 +23,7 @@ const Login = () => {
   return (
     <div className="auth_page">
       <form onSubmit={handleSubmit}>
-        <h3 className="text-uppercase">QudiSpace</h3>
+        <h3 className="text-uppercase text-center mb-4">QudiSpace</h3>
         <div className="form-group">
           <label htmlFor="exampleInputEmail1">Email</label>
           <input
