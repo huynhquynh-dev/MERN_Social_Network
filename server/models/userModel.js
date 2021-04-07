1a8cf7e9efa37b4eebe64bf1cf9880c4178e3fb1
huynhquynh-dev <huynhquynh-dev>
2021-04-07 10:47:41 +0700

    Server: Added user model

---
+++ server/models/userModel.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 25
+    },
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 25,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        default: 'https://png.pngtree.com/element_our/20200610/ourmid/pngtree-character-default-avatar-image_2237203.jpg'
+    },
+    role: {
+        type: String,
+        default: 'user'
+    },
+    gender: {
+        type: String,
+        default: 'male'
+    },
+    mobile: {
+        type: String,
+        default: ''
+    },
+    address: {
+        type: String,
+        default: ''
+    },
+    story: {
+        type: String,
+        default: '',
+        maxlength: 200
+    },
+    website: {
+        style: String,
+        default: ''
+    },
+    followers: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'user'
+        }
+    ],
+    following: [
+        {
+            type: mongoose.Types.ObjectId, 
+            ref: 'user'
+        }
+    ]
+}, {
+    time_stamp: true
+})
+
+module.exports = mongoose.model('user', userSchema)
