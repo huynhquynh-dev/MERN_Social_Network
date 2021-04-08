82e5e29bd8352045a78af7dc01a9902ff2ef855b
huynhquynh-dev <huynhquynh-dev>
2021-04-08 14:14:20 +0700

    Client: Added process refresh token when reload page with logined

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,31 @@
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Login from "./pages/login"
+import Login from "./pages/login";
+import Home from "./pages/home";
 
-import PageRender from './PageRender'
+import PageRender from "./PageRender";
 import Notify from "./components/notify/Notify";
- 
+
+import { useSelector, useDispatch } from "react-redux";
+
+import { refreshToken } from "./redux/actions/authAction"
+
 function App() {
+  const { auth } = useSelector((state) => state);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshToken())
+  },[dispatch])
+
   return (
     <Router>
       <Notify />
       <input type="checkbox" id="theme" />
       <div className="App">
         <div className="main">
-          <Route exact path="/" component={Login} />
+          <Route exact path="/" component={auth.token ? Home : Login} />
           <Route exact path="/:page" component={PageRender} />
           <Route exact path="/:page/:id" component={PageRender} />
         </div>
--- client/src/redux/actions/authAction.js
+++ client/src/redux/actions/authAction.js
@@ -39,3 +39,28 @@ export const login = (data) => async (dispatch) => {
         })
     }
 }
+
+export const refreshToken = () => async (dispatch) => {
+    const firstLogin = localStorage.getItem('firstLogin')
+    if (firstLogin){
+        dispatch({type: 'NOTIFY', payload: {loading: true}})
+        try {
+            const res = await postDataAPI('refresh_token')
+            dispatch({ 
+                type: 'AUTH',
+                payload: {
+                    token: res.data.access_token,
+                    user: res.data.user
+                }
+            })
+            dispatch({type: 'NOTIFY', payload: {}})
+        } catch (error) {
+            dispatch({ 
+                type: 'NOTIFY',
+                payload: {
+                    error: error.response.data.message
+                }
+            })
+        }
+    }
+}
