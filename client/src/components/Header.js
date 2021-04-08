ac2377a0cf91f02cf8bfea28ca7a7cd731c93f42
huynhquynh-dev <huynhquynh-dev>
2021-04-08 22:59:02 +0700

    Client: Added render menu

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,47 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 const Header = () => {
 
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between align-middle">
-        <a className="navbar-brand" href="/">
-            Qudispace
-        </a>
+    const navLinks = [
+        { label: "Home", icon: "home", path: "/" },
+        { label: "Message", icon: "near_me", path: "/message" },
+        { label: "Discover", icon: "explore", path: "/discover" },
+        { label: "Notify", icon: "favorite", path: "/notify" },
+    ];
 
-      <div className="menu">
-        <ul className="navbar-nav px-2 flex-row">
-            <li className="nav-item dropdown">
-                <a
-                    className="nav-link dropdown-toggle"
-                    href="/"
-                    id="navbarDropdown"
-                    role="button"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                >
-                    User
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="/">
-                        Profile
-                    </a>
-                    <a className="dropdown-item" href="/">
-                        Dark mode
-                    </a>
-                    <div className="dropdown-divider"></div>
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between align-middle">
+            <a className="navbar-brand" href="/">
+                Qudispace
+            </a>
+
+        <div className="menu">
+            <ul className="navbar-nav px-2 flex-row">
+                {navLinks.map((link, index) => (
+                    <li className="nav-item" key={index}>
+                        <Link className="nav-link active" to={link.path}>
+                            <span className="material-icons">{link.icon}</span>
+                        </Link>
+                    </li>
+                ))}
+                <li className="nav-item dropdown">
                     <a
-                        className="dropdown-item"
+                        className="nav-link dropdown-toggle"
                         href="/"
+                        id="navbarDropdown"
+                        role="button"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
                     >
-                        Logout
+                        User
                     </a>
-                </div>
-            </li>
-        </ul>
-      </div>
-    </nav>
-  );
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="dropdown-item" href="/">
+                            Profile
+                        </a>
+                        <a className="dropdown-item" href="/">
+                            Dark mode
+                        </a>
+                        <div className="dropdown-divider"></div>
+                        <a
+                            className="dropdown-item"
+                            href="/"
+                        >
+                            Logout
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        </nav>
+    );
 };
 
 export default Header;
