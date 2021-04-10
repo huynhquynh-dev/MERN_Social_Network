d96d518d9d2ece39add3bb8ed188593ca95acf24
huynhquynh-dev <huynhquynh-dev>
2021-04-10 13:03:02 +0700

    Server: Fixed systax

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,7 +3,7 @@ const Users = require("../models/userModel");
 const userController = {
   searchUser: async (req, res) => {
     try {
-        const users = await User.find({ username: { $regex: req.query.username } })
+        const users = await Users.find({ username: { $regex: req.query.username } })
             .limit(10)
             .select("fullname username avatar");
 
