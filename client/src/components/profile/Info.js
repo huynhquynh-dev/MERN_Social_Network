4a6040b4c52eb18d7961ddbe64d3a821e7acd981
huynhquynh-dev <huynhquynh-dev>
2021-04-12 11:22:23 +0700

    Client: Added style for Info profile

--- client/src/components/profile/Info.js
+++ client/src/components/profile/Info.js
@@ -15,6 +15,8 @@ const Info = () => {
     useEffect(() => {
         if(id === auth.user._id){
             setUserData([auth.user])
+        } else {
+            setUserData([])
         }
     }, [id, auth.user])
 
@@ -30,14 +32,16 @@ const Info = () => {
                                 <button className="btn btn-outline-info">Edit profile</button>
                             </div>
                             <div className="info-content-body">
-                                <span>
-                                    { user.followers.length } Followers
-                                </span>
-                                <span>
-                                    { user.following.length } Following
-                                </span>
-                                <h6>{user.fullname}</h6>
-                                <p>{user.address}</p>
+                                <div className="follow-btn">
+                                    <span className="mr-4">
+                                        { user.followers.length } Followers
+                                    </span>
+                                    <span className="ml-4">
+                                        { user.following.length } Following
+                                    </span>
+                                </div>
+                                <h6>{user.fullname} {user.mobile}</h6>
+                                <p className="m-0">{user.address}</p>
                                 <h6>{user.email}</h6>
                                 <a href={user.website} target="_blank" rel="noreferrer">
                                     { user.website }
