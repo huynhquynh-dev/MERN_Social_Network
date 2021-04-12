23cc68a46db231fa33f9fc996b14882e431b13a7
huynhquynh-dev <huynhquynh-dev>
2021-04-12 21:17:15 +0700

    Client: Custom view search

--- client/src/components/header/Search.js
+++ client/src/components/header/Search.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getDataAPI } from "../../utils/fetchData";
-import { GLOBAL_TYPE } from '../../redux/actions/globalType'
-import UserCard from '../UserCard'
-import LoadIcon from '../../images/loading-circle.gif'
+import { GLOBAL_TYPE } from "../../redux/actions/globalType";
+import UserCard from "../UserCard";
+import LoadIcon from "../../images/loading-circle.gif";
 
 const Search = () => {
   const [search, setSearch] = useState();
@@ -26,25 +26,27 @@ const Search = () => {
   // }, [search, auth.token, dispatch]);
 
   const handleCloseSearch = () => {
-    setSearch('')
-    setUsers([])
-  }
+    setSearch("");
+    setUsers([]);
+  };
 
   const handleSearch = async (e) => {
-      e.preventDefault()
+    e.preventDefault();
 
-      if(!search) return
+    if (!search) return;
 
-      try {
-        setLoad(true)
-        const res = await getDataAPI(`search?username=${search}`, auth.token)
-        setUsers(res.data.users)
-        setLoad(false)
-      } catch (error) {
-        dispatch({type: GLOBAL_TYPE.ALERT, payload: {error: error.response.data.message}})
-      }
-  }
-  
+    try {
+      setLoad(true);
+      const res = await getDataAPI(`search?username=${search}`, auth.token);
+      setUsers(res.data.users);
+      setLoad(false);
+    } catch (error) {
+      dispatch({
+        type: GLOBAL_TYPE.ALERT,
+        payload: { error: error.response.data.message },
+      });
+    }
+  };
 
   return (
     <form className="search-form" onSubmit={handleSearch}>
@@ -52,33 +54,41 @@ const Search = () => {
         type="text"
         name="search"
         id="search"
-        value={search || ''}
+        title="Enter to Search"
+        value={search || ""}
         onChange={(e) =>
           setSearch(e.target.value.toLowerCase().replace(/ /g, ""))
         }
       />
-      <button type="submit" style={{display: 'none'}}>Search</button>
-
-      { load && <img className="loading-icon" src={LoadIcon} alt="loading" />}
 
       <div className="search-icon" style={{ opacity: search ? 0 : 0.3 }}>
         <span className="material-icons">search</span>
-        <span>search</span>
+        <span>Enter to Search</span>
       </div>
-      <div className="close-search" style={{opacity: users.length === 0 ? 0 : 1}} onClick={handleCloseSearch}>
+      <div
+        className="close-search"
+        style={{ opacity: users.length === 0 ? 0 : 1 }}
+        onClick={handleCloseSearch}
+      >
         &times;
       </div>
+
+      <button type="submit" style={{ display: "none" }}>
+        Search
+      </button>
+
+      {load && <img className="loading-icon" src={LoadIcon} alt="loading" />}
+
       <div className="users">
-        {
-            search && users.map(user => (
-              <UserCard 
-                key={user._id}
-                user={user} 
-                border="border"
-                handleCloseSearch={handleCloseSearch}  
-              />
-            ))
-        }
+        {search &&
+          users.map((user) => (
+            <UserCard
+              key={user._id}
+              user={user}
+              border="border"
+              handleCloseSearch={handleCloseSearch}
+            />
+          ))}
       </div>
     </form>
   );
